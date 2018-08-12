668cbef4c4b9579f0b400ed648a3b4c7576ac89b
Chris Speck <cgspeck@gmail.com>
2018-08-12 22:21:01 +1000

    wip: implement guts of custom step

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 import time
 
@@ -13,34 +14,42 @@ class AlarmClockStep(StepBase):
     start_minute = Property.Select("Start Minute", options=list(range(0, 60, 5)))
     force_off_at_start = Property.Select("Force off at start", options=["disabled", "enabled"], description="If enabled then this step will switch off the coils and pump when it starts.")
     kettle = StepProperty.Kettle("Kettle")
+    pump = StepProperty.Actor("Pump", description="Pump actor gets toogled")
 
     def __init__(self, *args, **kwds):
         StepBase.__init__(self, *args, **kwds)
         self._logger = logging.getLogger(type(self).__name__)
 
-    def stop(self):
-        '''
-        Invoked when the automatic is stopped.
-        Normally you switch off the actors and clean up everything
-        :return: None
-        '''
-        super(KettleController, self).stop()
-        # self.heater_off()
+    def init(self):
+        if self.mode == "enabled":
+            abstract_end_time = datetime.timedelta(
+                hours=int(self.start_hour),
+                minutes=int(self.start_minute)
+            )
+            dt_now = datetime.datetime.now()
+            abstract_now_time = datetime.timedelta(
+                hours=dt_now.hour,
+                minutes=dt_now.minute
+            )
 
+            # TODO: set the wakeup time
+            if abstract_now_time > abstract_end_time:
+                # actual end time is the next day
+            else:
+                # actual end time is today
 
-    def run(self):
-        enabled = self.mode == "enabled"
-        while self.is_running():
-            self._logger.debug("started")
+            self.end_time = ...
+            self.notify("Alarm clock enabled", "Brewing will continue at %s" % self.end_time, timeout=None)
 
-            if enabled:
-                self._logger.debug("alarm clock is enabled")
-                self.start_time = time.time()
-                self._logger.debug("start time: %s" % self.start_time)
+            if self.force_off_at_start == "enabled":
+                self.notify("Alarm clock enabled", "Turning pump and kettle off")
+                self.actor_off(int(self.pump))
+                self.set_target_temp(0, self.kettle)
+        else:
+            self.notify("Alarm clock not enabled!", "Starting the next step")
+            self.next()
 
-                self.end_time = ...
-                self._logger.debug("end time: %s" % self.start_time)
-
-                while time.time() < self.end_time:
-                    self._logger.debug("alarm clock sleeping")
-                    self.sleep(10)
+    def execute(self):
+        if time.time() >= self.end_time:
+            self.notify("Alarm clock triggered", "Starting the next step", timeout=None)
+            self.next()
